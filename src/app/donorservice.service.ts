7d2c368dd1b992e8c4832d336048039c5500841d
sravani251 <17211a04e5@bvrit.ac.in>
2021-05-17 16:48:42 +0530

    donorservice

--- src/app/donorservice.service.ts
+++ src/app/donorservice.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Donor } from './models/Donor';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +11,22 @@ export class DonorserviceService {
 
   baseURL='http://localhost:8085/donor';
 
-  constructor(private httpModule:HttpClient) { }
+  constructor(private httpClient:HttpClient) { }
+
+  registerDonor(donor:Donor):Observable<any>{
+    console.log("create donor ");
+    return this.httpClient.post(this.baseURL+'/donor/register',donor);
+}
+  login(username:string,password:string):Observable<any>{
+  console.log("login donor");
+  return this.httpClient.get(this.baseURL+'/donor/login',username,password);
+}
+  forgotPassword(username:string):Observable<any>{
+    console.log("");
+    return this.httpClient.get(this.baseURL+'/donor/forgot_password'+username);
+}
+  resetPassword(username:string):Observable<any>{
+  console.log("");
+  return this.httpClient.put(this.baseURL+'/donor/reset_password'+username);
+}
 }
