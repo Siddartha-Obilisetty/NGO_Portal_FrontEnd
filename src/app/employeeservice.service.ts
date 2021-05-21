aec30c73f9bc0f6c2eafd0f3542fcb55022b019b
varun3k <17211a05a8@bvrit.ac.in>
2021-05-21 15:55:03 +0530

    commit

--- src/app/employeeservice.service.ts
+++ src/app/employeeservice.service.ts
@@ -21,18 +21,8 @@ export class EmployeeserviceService {
   }
   handleError(errorResponse: HttpErrorResponse)
   {
-    let errorMessage = 'An error occurred';
-    
     console.log(errorResponse.error);
-    switch(errorResponse.error[0]){
-      case 'T':
-        errorMessage='No Such Employee';
-        break;
-      case 'W':
-        errorMessage='Wrong Credentials!!!';
-        break;
-    }
-    return throwError(errorMessage); 
+    return throwError(errorResponse.error); 
   }  
   
   addNeedyPerson(needyPeople:NeedyPeople):Observable<any>{
@@ -40,21 +30,20 @@ export class EmployeeserviceService {
     return this.httpClient.post(this.baseURL+'/needypeople/add',needyPeople).pipe(catchError(this.handleError1));
   }
   handleError1(errorResponse1: HttpErrorResponse){
-    let errorMessage1 = "An error occured";
     console.log(errorResponse1.error);
-    if(errorResponse1.error[0]){
-  
-        errorMessage1='Duplicate Needy People';
-        
-    }
-    return throwError(errorMessage1);   
+    return throwError(errorResponse1.error);   
   }
 
   removeNeedyPerson(needyPeopleId:number):Observable<any>{
     console.log("delete needyperson in service");
-    return this.httpClient.delete(this.baseURL+'/needypeople/delete/'+needyPeopleId);
+    return this.httpClient.delete(this.baseURL+'/needypeople/delete/'+needyPeopleId).pipe(catchError(this.handleError4));;
   }
+  handleError4(errorResponse4: HttpErrorResponse){
+    console.log(errorResponse4.error);
+    return throwError(errorResponse4.error);   
 
+  }
+  
   findAllNeedyPeople():Observable<any>{
     console.log("find all needyperson in service");
     return this.httpClient.get(this.baseURL+'/needypeople/all');
@@ -62,12 +51,22 @@ export class EmployeeserviceService {
 
   findNeedyPeopleById(needyPeopleId:number):Observable<any>{
     console.log("get needyperson by id in service");
-    return this.httpClient.get(this.baseURL+'/needypeople/getbyId/'+needyPeopleId);
+    return this.httpClient.get(this.baseURL+'/needypeople/getbyId/'+needyPeopleId).pipe(catchError(this.handleError2));
+  }
+  handleError2(errorResponse2:HttpErrorResponse){
+    console.log(errorResponse2.error);
+    return throwError(errorResponse2.error);   
+
   }
 
   findNeedyPeopleByName(needyPeopleName:string):Observable<any>{
     console.log("get needyperson by Name in service");
-    return this.httpClient.get(this.baseURL+'/needypeople/getbyName/'+needyPeopleName);
+    return this.httpClient.get(this.baseURL+'/needypeople/getbyName/'+needyPeopleName).pipe(catchError(this.handleError3));
+  }
+  handleError3(errorResponse3: HttpErrorResponse){
+    console.log(errorResponse3.error);
+    return throwError(errorResponse3.error);   
+
   }
 
 
