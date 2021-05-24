5e7e6568233c71f078a3a0f3b467c8024b644f42
Sai Siddartha Obilisetty <siddarthaobilisetty@gmail.com>
2021-05-24 15:09:52 +0530

    commit

--- src/app/employee/employee-nav/employee-nav.component.ts
+++ src/app/employee/employee-nav/employee-nav.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AdminserviceService } from 'app/adminservice.service';
+import { EmployeeserviceService } from 'app/employeeservice.service';
+import { Employee } from 'app/models/Employee';
 
 @Component({
   selector: 'app-employee-nav',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeNavComponent implements OnInit {
 
-  constructor() { }
+  employee:Employee=new Employee();
+  empid:number;
+
+  constructor(private router:Router,private employeeService:EmployeeserviceService,private route:ActivatedRoute,private adminService:AdminserviceService) { }
 
   ngOnInit(): void {
+    this.empid=this.route.snapshot.params['id'];
+    this.adminService.findEmployeeById(this.empid).subscribe(
+      employee=>{
+        this.employee=employee;
+      }
+    );
   }
-
+  goToProfile(){
+    this.router.navigate(['employee',this.empid,'profile']);
+  }
 }
