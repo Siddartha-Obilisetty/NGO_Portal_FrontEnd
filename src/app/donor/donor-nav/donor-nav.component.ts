0f404ba733f510973c1aa620468c2b4ab87b95af
Sai Siddartha Obilisetty <siddarthaobilisetty@gmail.com>
2021-05-24 16:13:36 +0530

    commit

--- src/app/donor/donor-nav/donor-nav.component.ts
+++ src/app/donor/donor-nav/donor-nav.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AdminserviceService } from 'app/adminservice.service';
+import { DonorserviceService } from 'app/donorservice.service';
+import { Donor } from 'app/models/Donor';
 
 @Component({
   selector: 'app-donor-nav',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonorNavComponent implements OnInit {
 
-  constructor() { }
+  donor:Donor=new Donor();
+  donorId:number;
+
+  constructor(private router:Router, private donorService:DonorserviceService,private route:ActivatedRoute,private adminService:AdminserviceService) { }
 
   ngOnInit(): void {
+    this.donorId=this.route.snapshot.params['id'];
+    this.adminService.findDonorById(this.donorId).subscribe(
+      donor=>{
+        this.donor=donor;
+      }
+    );
+  }
+
+  
+  goToProfile(){
+    this.router.navigate(['donor',this.donorId,'profile']);
   }
 
 }
--- src/app/needypeople/needypeople-nav/needypeople-nav.component.ts
+++ src/app/needypeople/needypeople-nav/needypeople-nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeeserviceService } from 'app/employeeservice.service';
+import { NeedyPeople } from 'app/models/NeedyPeople';
 
 @Component({
   selector: 'app-needypeople-nav',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NeedypeopleNavComponent implements OnInit {
 
-  constructor() { }
+  person:NeedyPeople=new NeedyPeople();
+  needyPersonId:number;
+
+  constructor(private router:Router,
+              private route:ActivatedRoute,
+              private empService:EmployeeserviceService) { }
 
   ngOnInit(): void {
+    this.needyPersonId=this.route.snapshot.params['id'];
+    this.empService.findNeedyPeopleById(this.needyPersonId).subscribe(
+      person=>{
+        this.person=person;
+      }
+    );
+  }
+  
+  goToProfile(){
+    this.router.navigate(['/needypeople',this.needyPersonId,'profile']);
   }
 
 }
