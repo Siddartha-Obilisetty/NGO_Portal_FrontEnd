527f96821eb8ffcbd1fd53289e6ee98e5ce639b4
varun3k <17211a05a8@bvrit.ac.in>
2021-05-24 16:14:34 +0530

    commit

--- src/app/donor/donor-nav/donor-nav.component.ts
+++ src/app/donor/donor-nav/donor-nav.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AdminserviceService } from 'app/adminservice.service';
+import { DonorserviceService } from 'app/donorservice.service';
+import { Donor } from 'app/models/Donor';
 
 @Component({
   selector: 'app-donor-nav',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./donor-nav.component.css']
 })
 export class DonorNavComponent implements OnInit {
+  donor:Donor = new Donor;
+  donorId:number;
 
-  constructor() { }
+  constructor(private router:Router,private donorService:DonorserviceService,private route:ActivatedRoute,private adminService:AdminserviceService) { }
 
   ngOnInit(): void {
+    this.donorId=this.route.snapshot.params['id'];
+    this.adminService.findDonorById(this.donorId).subscribe(
+      donor=>{
+        this.donor=donor;
+      }
+    );
   }
-
+  goToProfile(){
+    this.router.navigate(['employee',this.donorId,'profile']);
+  }
 }
